5e56c48cce630cff529e39ae70167c2ac23e8ad1
Cesar Picazo <70867430+cpicazo8304@users.noreply.github.com>
2026-03-29 14:27:57 -0500

    Add dataset generation functionality in datagen.py
    Implemented dataset generation with reasoning using CoTModel.

--- homework3_v3/homework/datagen.py
+++ homework3_v3/homework/datagen.py
@@ -1,5 +1,28 @@
 def generate_dataset(output_json: str, oversample: int = 10, temperature: float = 0.6):
-    raise NotImplementedError()
+    import json
+    from .cot import CoTModel
+    from .data import Dataset
+
+    # get dataset and llm
+    llm = CoTModel()
+    data = Dataset("train")
+    results = []
+
+    # go through each example in training dataset to create the same example but with
+    # reasoning
+    for question, answer in data:
+      prompt = [llm.format_prompt(question)]
+      completions = llm.batched_generate(prompt,num_return_sequences=oversample, temperature=temperature)
+
+      # go through each of the completions to see if the answer is correct (only need one)
+      for completion in completions:
+        if llm.parse_answer(completion) == float(answer):
+          results.append([question, float(answer), completion])
+          break
+
+    # store the output
+    with open(output_json, "w") as f:
+        json.dump(results, f, indent=2)
 
 
 if __name__ == "__main__":
