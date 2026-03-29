7ac98854ba27174acf2cbaa912f608fb838363e6
Cesar Picazo <70867430+cpicazo8304@users.noreply.github.com>
2026-03-29 14:27:31 -0500

    Add training functionality with LoRA and dataset generation
    Implemented model training with LoRA configuration and dataset generation.

--- homework3_v3/homework/rft.py
+++ homework3_v3/homework/rft.py
@@ -1,6 +1,6 @@
 from .base_llm import BaseLLM
-from .sft import test_model
-
+from .sft import test_model, format_example, TokenizedDataset
+from .datagen import generate_dataset
 
 def load() -> BaseLLM:
     from pathlib import Path
@@ -21,8 +21,51 @@ def train_model(
     output_dir: str,
     **kwargs,
 ):
-    # Reuse much of the SFT code here
-    raise NotImplementedError()
+    import json
+    from peft import get_peft_model, LoraConfig
+    from transformers import TrainingArguments, Trainer
+    from pathlib import Path
+    # create LoRA model
+    llm = BaseLLM()
+
+    config = LoraConfig(
+        task_type="CAUSAL_LM",
+        bias="none",
+        target_modules="all-linear",
+        r=32,
+        lora_alpha=128,
+    )
+
+    peft_model = get_peft_model(llm.model, config)
+    peft_model.enable_input_require_grads()
+
+    training_args = TrainingArguments(
+      gradient_checkpointing=True,
+      learning_rate=5e-4,
+      output_dir=output_dir,
+      logging_dir=output_dir,
+      report_to="tensorboard",
+      num_train_epochs=5,
+      per_device_train_batch_size=32
+    )
+
+    # create dataset
+    generate_dataset('data/rft.json', 20)
+    rft_data = json.load(open(Path(__file__).parent.parent / "data" / "rft.json"))
+    dataset = TokenizedDataset(llm.tokenizer, rft_data, format_example)
+
+    # initialize trainer with args, dataset, and 
+    trainer = Trainer(
+        peft_model,
+        training_args,
+        train_dataset=dataset
+    )
+
+    # train model
+    trainer.train()
+    trainer.save_model("homework/rft_model")
+
+    test_model("homework/rft_model")
 
 
 if __name__ == "__main__":
