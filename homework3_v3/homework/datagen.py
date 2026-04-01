ef940cc2288414651ebe7fdcd50671391d084151
Cesar Picazo <70867430+cpicazo8304@users.noreply.github.com>
2026-03-31 19:31:14 -0500

    Refactor batch size handling in dataset generation

--- homework3_v3/homework/datagen.py
+++ homework3_v3/homework/datagen.py
@@ -2,19 +2,22 @@ def generate_dataset(output_json: str, oversample: int = 10, temperature: float 
     import json
     from .cot import CoTModel
     from .data import Dataset
+    import torch
 
     llm = CoTModel(checkpoint="HuggingFaceTB/SmolLM2-1.7B-Instruct")
     data = list(Dataset("train"))
     results = []
     question_counts = {}
     prompts = [llm.format_prompt(q) for q, _ in data]
+    batch_size = 32
+    print("Batch size: ", batch_size)
 
     while len(results) < 1000:
-      for i in range(0, len(data), 32):  # process 32 questions at a time
+      for i in range(0, len(data), batch_size):  # process 32 questions at a time
           if len(results) >= 1000:
               break
-          batch = data[i:i+32]
-          all_completions = llm.batched_generate(prompts[i:i+32], num_return_sequences=oversample, temperature=temperature)
+          batch = data[i:i+batch_size]
+          all_completions = llm.batched_generate(prompts[i:i+batch_size], num_return_sequences=oversample, temperature=temperature)
 
           for (question, answer), completions in zip(batch, all_completions):
               if len(results) >= 1000:
@@ -27,8 +30,9 @@ def generate_dataset(output_json: str, oversample: int = 10, temperature: float 
                       results.append([question, true, completion])
                       question_counts[question] = question_counts.get(question, 0) + 1
                       break
+              torch.cuda.empty_cache()
 
-      print(f"Generated {len(results)} examples so far")
+          print(f"Generated {len(results)} examples so far")
 
     with open(output_json, "w") as f:
         json.dump(results, f, indent=2)
